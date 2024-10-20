89d15a7fa0d69561ed376d07636fb12fd0094c2a
inf0rmatix <kontakt@paul-koehler.dev>
2024-10-20 12:56:44 +0200

    fix: fixing some stuff and adjusting documentation

--- vps_deployment_flutter/lib/main.dart
+++ vps_deployment_flutter/lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:vps_deployment_client/vps_deployment_client.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_flutter/serverpod_flutter.dart';
+import 'package:vps_deployment_client/vps_deployment_client.dart';
 
 // Sets up a singleton client object that can be used to talk to the server from
 // anywhere in our app. The client is generated from your server code.
 // The client is set up to connect to a Serverpod running on a local server on
 // the default port. You will need to modify this to connect to staging or
 // production servers.
-var client = Client('http://$localhost:8080/')
+var client = Client('https://api.paul-koehler.dev/')
   ..connectivityMonitor = FlutterConnectivityMonitor();
 
 void main() {
